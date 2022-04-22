2b83e4963a638ad67bf6928cdb5ca1252bb91771
pandeyprafull <prafullpandey68@gmail.com>
2022-04-22 11:20:49 +0530

    Routing + Detail Card component + Form

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,11 @@
+// our-domain.com/new-meetup.com
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+function NewMeetupPage() {
+    function addMeetupHandler(enteredMeetupData) {
+        console.log(enteredMeetupData);
+    }
+    return <NewMeetupForm onAddMeetup={addMeetupHandler} />
+}
+
+export default NewMeetupPage;
