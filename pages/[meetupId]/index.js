4a9955d9055bbc72598ecc56ea83f343ba3de452
pandeyprafull <prafullpandey68@gmail.com>
2022-04-26 12:50:39 +0530

    getStaticPath understanding for dynaic + static pager prerendering

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,6 +1,6 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDeatils() {
+function MeetupDeatils(props) {
     return (
         <MeetupDetail
             image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
@@ -9,6 +9,40 @@ function MeetupDeatils() {
             description="This Is a First Meetup"
         />
     )
+}
+
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1'
+                }
+            },
+            {
+                params: {
+                    meetupId: 'm2'
+                }
+            }
+        ]
+    }
+}
+
+export async function getStaticProps(context) {
+    const meetupId = context.params.meetupId;
+    console.log("--->", meetupId)
+    return {
+        props: {
+            meetupData: {
+                id: meetupId,
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+                title: "First Meetup",
+                address: "Some Street 5, Some City",
+                description: "This Is a First Meetup"
+            }
+        }
+    }
 }
 
 export default MeetupDeatils;
